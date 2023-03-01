6d304d7a25e484ddb1cd1625f4fae6aae69defef
SvetaK7 <atevs7@inbox.ru>
2023-03-01 22:44:32 +0300

    add HashRouter

--- src/pages/Pages.tsx
+++ src/pages/Pages.tsx
@@ -11,18 +11,18 @@ import Error404 from "./Error404";
 
 export const Pages = () => {
     return (
-        <Routes>
-            <Route path={'/'} element={<Navigate to={PATH.TEST_SUPER_COMPONENTS}/>}/>
-            <Route path={PATH.LOGIN} element={<Login/>}/>
-            <Route path={PATH.REGISTRATION} element={<Registration/>}/>
-            <Route path={PATH.RECOVERY} element={<Recovery/>}/>
-            <Route path={PATH.PROFILE} element={<Profile/>}/>
-            <Route path={PATH.NEW_PASSWORD} element={<NewPassword/>}/>
-            <Route path={PATH.TEST_SUPER_COMPONENTS} element={<TestSuperComponents/>}/>
-            {/*<Route path={'*'} element={<Error404/>}/>*/}
-            <Route path={'/404'} element={<Error404/>}/>
-            <Route path={'*'} element={<Navigate to={'/404'}/>}/>
-        </Routes>
+            <Routes>
+                <Route path={'/'} element={<Navigate to={PATH.TEST_SUPER_COMPONENTS}/>}/>
+                <Route path={PATH.LOGIN} element={<Login/>}/>
+                <Route path={PATH.REGISTRATION} element={<Registration/>}/>
+                <Route path={PATH.RECOVERY} element={<Recovery/>}/>
+                <Route path={PATH.PROFILE} element={<Profile/>}/>
+                <Route path={PATH.NEW_PASSWORD} element={<NewPassword/>}/>
+                <Route path={PATH.TEST_SUPER_COMPONENTS} element={<TestSuperComponents/>}/>
+                {/*<Route path={'/*'} element={<Error404/>}/>*/}
+                <Route path={'/404'} element={<Error404/>}/>
+                <Route path={'*'} element={<Navigate to={'/404'}/>}/>
+            </Routes>
     )
 }
 
