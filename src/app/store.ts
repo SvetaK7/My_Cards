d0c6f14c88fcb57357a900d6cc62bf4fa21023ab
SvetaK7 <atevs7@inbox.ru>
2023-03-05 00:21:13 +0300

    feat: add RTK, store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,20 @@
 import {applyMiddleware, combineReducers, legacy_createStore} from "redux";
 import thunkMiddleware from 'redux-thunk'
 import {authReducer} from "../features/auth/auth-reducer";
+import {configureStore} from "@reduxjs/toolkit";
+import {TypedUseSelectorHook, useDispatch, useSelector} from "react-redux";
 
 const rootReducer = combineReducers({
     auth: authReducer
 })
-export const store = legacy_createStore(rootReducer, applyMiddleware(thunkMiddleware));
-export type AppRootStateType = ReturnType<typeof rootReducer>
+
+export const store = configureStore({
+    reducer: rootReducer,
+    middleware: getDefaultMiddleware => getDefaultMiddleware().prepend(thunkMiddleware)
+})
+
+export const useAppDispatch:() => AppDispatch=useDispatch
+export type AppDispatch = typeof store.dispatch
+export const useAppSelector: TypedUseSelectorHook<StateType> = useSelector
+
+export type StateType = ReturnType<typeof rootReducer>;
